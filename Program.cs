61d91ddc63ebb49b57e2ed3aa5450acb8d2f7a4b
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2022-03-31 15:37:54 +0700

    add SHA256 and checkSpecialChar to prevent injection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace ATBM_DOAN01
 {
     internal static class Program
@@ -12,6 +17,32 @@ namespace ATBM_DOAN01
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Login());
+        }
+
+        
+    }
+
+    public class Hash
+    {
+        public static string getHashSha256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+
+    public class checkInput
+    {
+        public static bool hasSpecialCharacter(string input)
+        {
+            return !Regex.IsMatch(input, "^[a-zA-Z0-9]*$");
         }
     }
 }
