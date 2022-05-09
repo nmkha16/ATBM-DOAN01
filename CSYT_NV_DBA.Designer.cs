01f02e30ab9564344d45677a3c608ff5140e73d9
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2022-05-10 01:19:21 +0700

    Xong TC#1 cho phép dba cập nhật CSYT và NHANVIEN
    Còn 1 phần connect through

---
+++ CSYT_NV_DBA.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ATBM_DOAN01
+{
+    partial class CSYT_NV_DBA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database Administrator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bảng";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "NHÂN VIÊN",
+            "CSYT"});
+            this.comboBox1.Location = new System.Drawing.Point(52, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(870, 252);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 351);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(870, 168);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cập nhật";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 22);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 90;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(858, 224);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 22);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 90;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView2.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(858, 95);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(771, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(672, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 36);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(573, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 36);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Xoá";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CSYT_NV_DBA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 551);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CSYT_NV_DBA";
+            this.Text = "CSYT_NV_DBA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView2;
+    }
+}
---
+++ CSYT_NV_DBA.cs
@@ -0,0 +1,266 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_DOAN01
+{
+    public partial class CSYT_NV_DBA : Form
+    {
+        private Interface _itf;
+        private OracleConnection _con;
+        private DataTable data, dataCN;
+        public CSYT_NV_DBA(Interface itf,OracleConnection con)
+        {
+            _itf = itf;
+            _con = con;
+            InitializeComponent();
+            FormClosing += CSYT_NV_DBA_FormClosing;
+        }
+
+        private void CSYT_NV_DBA_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            _itf.Show();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+
+        private void refresh()
+        {
+            string query = "";
+            if (comboBox1.SelectedIndex == 0) // bảng nhân viên
+            {
+                query = "select * from nhanvien";
+            }
+            else if (comboBox1.SelectedIndex == 1) // bảng CSYT
+            {
+                query = "select * from csyt";
+            }
+
+
+            try
+            {
+                data.Clear();
+            }
+            catch { }
+
+
+            OracleCommand command = new OracleCommand(query, _con);
+            OracleDataReader reader = command.ExecuteReader();
+            data = new DataTable();
+            data.Load(reader);
+
+            dataGridView1.DataSource = data;
+        }
+
+        /// <summary>
+        /// nút thêm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query;
+            OracleCommand command = null;
+            List<string?> list = new List<string?>();
+            if (comboBox1.SelectedIndex == 0) // bảng nhân viên
+            {              
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                {
+                    list.Add(dataGridView2.Rows[0].Cells[i].Value.ToString());
+                }
+
+                query = "insert into nhanvien(manv,hoten,phai,ngaysinh,cmnd,quequan,sodt,csyt,vaitro,makhoa) " +
+                    "values(:manv,:hoten,:phai,:ngaysinh,:cmnd,:quequan,:sodt,:csyt,:vaitro,:makhoa)";
+
+                command = new OracleCommand(query, _con);
+                command.Parameters.Add("manv",list[0] == null ? DBNull.Value : list[0].ToUpper());
+                command.Parameters.Add("hoten", list[1] == null ? DBNull.Value : list[1]);
+                command.Parameters.Add("phai", list[2] == null ? DBNull.Value : list[2]);
+                command.Parameters.Add("ngaysinh", list[3] == null ? DBNull.Value : DateTime.Parse(list[3]));
+                command.Parameters.Add("cmnd", list[4] == null ? DBNull.Value : list[4]);
+                command.Parameters.Add("quequan", list[5] == null ? DBNull.Value : list[5]);
+                command.Parameters.Add("sodt", list[6] == null ? DBNull.Value : list[6]);
+                command.Parameters.Add("csyt", list[7] == null ? DBNull.Value : list[7].ToUpper());
+                command.Parameters.Add("vaitro", list[8] == null ? DBNull.Value : list[8]);
+                command.Parameters.Add("makhoa", list[9] == null ? DBNull.Value : list[9].ToUpper());
+            }
+            else if (comboBox1.SelectedIndex == 1) // bảng CSYT
+            {
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                {
+                    list.Add(dataGridView2.Rows[0].Cells[i].Value.ToString());
+                }
+                query = "insert into csyt(macsyt,tencsyt,dccsyt,sdtcsyt) " +
+                    "values (:macsyt,:tencsyt,:dccsyt,:sdtcsyt)";
+                command = new OracleCommand(query, _con);
+                command.Parameters.Add("macsyt", list[0] == null ? DBNull.Value : list[0].ToUpper());
+                command.Parameters.Add("tencsyt", list[1] == null ? DBNull.Value : list[1]);
+                command.Parameters.Add("dccsyt", list[2] == null ? DBNull.Value : list[2]);
+                command.Parameters.Add("sdtcsyt", list[3] == null ? DBNull.Value : list[3]);
+            }
+
+            if (command == null) return;
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Thêm dữ liệu thành công!", "Thông báo");
+                refresh();
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// nút sửa
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "";
+            OracleCommand command = null;
+            List<string?> list = new List<string?>();
+
+            if (comboBox1.SelectedIndex == 0) // bảng nhân viên
+            {
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                {
+                    list.Add(dataGridView2.Rows[0].Cells[i].Value.ToString());
+                }
+
+                query = "update nhanvien " +
+                    "set manv = :manv," +
+                    "hoten = :hoten," +
+                    "phai = :phai," +
+                    "ngaysinh = :ngaysinh," +
+                    "cmnd = :cmnd," +
+                    "quequan = :quequan," +
+                    "sodt = :sodt," +
+                    "csyt = :csyt," +
+                    "vaitro = :vaitro," +
+                    "makhoa = :makhoa " +
+                    "where manv = :manv1";
+
+                command = new OracleCommand(query, _con);
+
+                command.Parameters.Add("manv", list[0] == null ? DBNull.Value : list[0].ToUpper());
+                command.Parameters.Add("hoten", list[1] == null ? DBNull.Value : list[1]);
+                command.Parameters.Add("phai", list[2] == null ? DBNull.Value : list[2]);
+                command.Parameters.Add("ngaysinh", list[3] == null ? DBNull.Value : DateTime.Parse(list[3]));
+                command.Parameters.Add("cmnd", list[4] == null ? DBNull.Value : list[4]);
+                command.Parameters.Add("quequan", list[5] == null ? DBNull.Value : list[5]);
+                command.Parameters.Add("sodt", list[6] == null ? DBNull.Value : list[6]);
+                command.Parameters.Add("csyt", list[7] == null ? DBNull.Value : list[7].ToUpper());
+                command.Parameters.Add("vaitro", list[8] == null ? DBNull.Value : list[8]);
+                command.Parameters.Add("makho", list[9] == null ? DBNull.Value : list[9].ToUpper());
+                command.Parameters.Add("manv1",list[0] == null ? DBNull.Value : list[0].ToUpper());
+            }
+            else if (comboBox1.SelectedIndex == 1) // bảng CSYT
+            {
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                {
+                    list.Add(dataGridView2.Rows[0].Cells[i].Value.ToString());
+                }
+                query = "update csyt " +
+                    "set macsyt = :macsyt," +
+                    "tencsyt = :tencsyt," +
+                    "dccsyt = :dccsyt," +
+                    "sdtcsyt = :sdtcsyt " +
+                    "where macsyt = :macsyt1";
+
+                command = new OracleCommand(query, _con);
+
+                command.Parameters.Add("macsyt", list[0] == null ? DBNull.Value : list[0].ToUpper());
+                command.Parameters.Add("tencsyt", list[1] == null ? DBNull.Value : list[1]);
+                command.Parameters.Add("dccsyt", list[2] == null ? DBNull.Value : list[2]);
+                command.Parameters.Add("sdtcsyt", list[3] == null ? DBNull.Value : list[3]);
+                command.Parameters.Add("macsyt1", list[0] == null ? DBNull.Value : list[0].ToUpper());
+
+            }
+
+            if (command == null) return;
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Cập nhật dữ liệu thành công", "Thông báo");
+                refresh();
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// nút xoá
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string query = "";
+            
+            if (comboBox1.SelectedIndex == 0) // bảng nhân viên
+            {
+                query = "delete from nhanvien where manv = '" + dataGridView2.Rows[0].Cells[0].Value.ToString()+"'";
+            }
+            else if (comboBox1.SelectedIndex == 1) // bảng CSYT
+            {
+                query = "delete from csyt where macsyt = '" + dataGridView2.Rows[0].Cells[0].Value.ToString() + "'";
+            }
+            OracleCommand command = new OracleCommand(query,_con);
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Xoá dữ liệu thành công", "Thông báo");
+                refresh();
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string query = "";
+            if (comboBox1.SelectedIndex == 0) // bảng nhân viên
+            {
+                query = "select * from nhanvien where manv = '" + dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()+"'";
+            }
+            else if (comboBox1.SelectedIndex == 1) // bảng CSYT
+            {
+                query = "select * from csyt where macsyt = '" + dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString() + "'";
+            }
+
+            try
+            {
+                dataCN.Clear();
+            }
+            catch { }
+
+
+            OracleCommand command = new OracleCommand(query, _con);
+            OracleDataReader reader = command.ExecuteReader();
+            dataCN = new DataTable();
+            dataCN.Load(reader);
+
+            dataGridView2.DataSource = dataCN;
+        }
+    }
+}
